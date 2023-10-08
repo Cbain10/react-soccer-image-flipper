fe2f87888d74b2d0c652a50997ce6d2f8fee9862
Cbain10 <cbain10.10@gmail.com>
2023-10-07 22:07:05 -0600

    dynammic markup

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import './MainPage.css';
-import { data } from '../data/data';
+import { data, logoArray, teamArray } from '../data/data';
 
 export const MainPage = () => {
+
+    const logos = logoArray;
+    const teams = teamArray;
 
     const [barcaURL, setBarcaUrl] = useState(data.barcaLogo);
     const [cityURL, setCityUrl] = useState(data.cityLogo);
@@ -14,6 +17,34 @@ export const MainPage = () => {
     const [liverpoolURL, setLiverpoolURL] = useState(data.liverpoolLogo);
     const [juventusURL, setJuventusURL] = useState(data.juventusLogo);
 
+    const stringArray = ['barca', 'city', 'bayern', 'chelsea', 'inter', 'brighton', 'united', 'liverpool', 'juventus'];
+    const stateArray = [barcaURL, cityURL, bayernURL, chelseaURL, interURL, brightonURL, unitedURL, liverpoolURL, juventusURL];
+
+    const getSetterFunction = (club) => {
+        switch(club) {
+            case 'barca':
+                return setBarcaUrl;
+            case 'city':
+                return setCityUrl;
+            case 'bayern':
+                return setBayernURL;
+            case 'chelsea':
+                return setChelseaURL;
+            case 'inter':
+                return setInterURL;
+            case 'brighton':
+                return setBrightonURL;
+            case 'united':
+                return setUnitedURL;
+            case 'liverpool':
+                return setLiverpoolURL;
+            case 'juventus':
+                return setJuventusURL;
+            default:
+                break;
+        }
+    }
+
 
     const handleChangeImage = (url, setState) => {
         setTimeout(() => {
@@ -24,90 +55,22 @@ export const MainPage = () => {
     return (
         <div>
             <h1>Image Flipper</h1>
-            <img
-                className='barca-img'
-                src={barcaURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.barcaTeam, setBarcaUrl)}
-                onMouseLeave={() => handleChangeImage(data.barcaLogo, setBarcaUrl)}
-                alt='barca'
-            />
-            <img
-                className='city-img'
-                src={cityURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.cityTeam, setCityUrl)}
-                onMouseLeave={() => handleChangeImage(data.cityLogo, setCityUrl)}
-                alt='city'
-            />
-            <img
-                className='bayern-img'
-                src={bayernURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.bayernTeam, setBayernURL)}
-                onMouseLeave={() => handleChangeImage(data.bayernLogo, setBayernURL)}
-                alt='bayern'
-            />
-            <br/>
-            <img
-                className='chelsea-img'
-                src={chelseaURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.chelseaTeam, setChelseaURL)}
-                onMouseLeave={() => handleChangeImage(data.chelseaLogo, setChelseaURL)}
-                alt='chelsea'
-            />
-            <img
-                className='inter-img'
-                src={interURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.interTeam, setInterURL)}
-                onMouseLeave={() => handleChangeImage(data.interLogo, setInterURL)}
-                alt='inter'
-            />
-            <img
-                className='brighton-img'
-                src={brightonURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.brightonTeam, setBrightonURL)}
-                onMouseLeave={() => handleChangeImage(data.brightonLogo, setBrightonURL)}
-                alt='brighton'
-            />
-            <br/>
-            <img
-                className='united-img'
-                src={unitedURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.unitedTeam, setUnitedURL)}
-                onMouseLeave={() => handleChangeImage(data.unitedLogo, setUnitedURL)}
-                alt='united'
-            />
-            <img
-                className='liverpool-img'
-                src={liverpoolURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.liverpoolTeam, setLiverpoolURL)}
-                onMouseLeave={() => handleChangeImage(data.liverpoolLogo, setLiverpoolURL)}
-                alt='liverpool'
-            />
-            <img
-                className='juventus-img'
-                src={juventusURL}
-                height={200}
-                width={200}
-                onMouseOver={() => handleChangeImage(data.juventusTeam, setJuventusURL)}
-                onMouseLeave={() => handleChangeImage(data.juventusLogo, setJuventusURL)}
-                alt='juventus'
-            />
-
+            {logos.map((logo, index) => {
+                return <>
+                            <img
+                                src={stateArray[index]}
+                                height={200}
+                                width={200}
+                                onMouseOver={() => handleChangeImage(teams[index], getSetterFunction(stringArray[index]))}
+                                onMouseLeave={() => handleChangeImage(logo, getSetterFunction(stringArray[index]))}
+                                alt={stringArray[index]}
+                            />
+                            {(index+1) % 3 === 0 &&
+                                <br />
+                            }
+                        </>
+            })
+            }
         </div>
     )
 }
